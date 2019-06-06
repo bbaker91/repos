c3701368a6fb8f9ff8042666a4ea007770b4045e
bbaker91 <46760895+bbaker91@users.noreply.github.com>
2019-06-06 09:49:08 -0700

    intial push

---
+++ Data_Layer/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Data_Layer
+{
+    public class Class1
+    {
+        public static List<Customers> GetList(string listType)
+        {
+            using (var context = new UWPC_TestEntities())
+            {
+                // setting parameter to pass to store procedure
+                var listTypeParameter = new SqlParameter("@ListType", listType);
+
+                
+                // Storing results of store procedure
+                var result = context.Database.SqlQuery<Customers>("splocal_ReturnCustomerList @ListType", listTypeParameter).ToList();
+
+                return result;
+            }
+        }
+
+    }
+
+        #region Model
+        public class Customers
+        {
+            public int id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string EmailAddress { get; set; }
+            public int frid01 { get; set; }
+            public int frid02 { get; set; }
+            public string EmailType { get; set; }
+            public string EmailTypeName { get; set; }
+            public int EmailId { get; set; }
+            public string Salutation { get; set; }
+        }
+    }
+    #endregion 
+
+
